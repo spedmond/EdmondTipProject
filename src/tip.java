d85183bad5b142103f7eb033f6fe2f13a485efa9
spedmond <songedmond777@gmail.com>
2022-09-28 12:26:50 -0400

    Initial commit

---
+++ src/tip.java
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+public class tip {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bill: ");
+        double bill = input.nextDouble();
+
+        System.out.
+    }
+}
