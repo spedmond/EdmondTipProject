f481337710b03015e5dac9d78b85231a1b0a1a2f
spedmond <114600476+spedmond@users.noreply.github.com>
2022-09-28 12:30:17 -0400

    Update tip.java

--- src/tip.java
+++ src/tip.java
@@ -1,12 +1,34 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class tip {
     public static void main(String[] args)
     {
+        //Getting values for initial variables
         Scanner input = new Scanner(System.in);
-        System.out.print("Bill: ");
+        System.out.print("Bill: $");
         double bill = input.nextDouble();
+        System.out.print("Amount of people: ");
+        int people = input.nextInt();
+        System.out.print("Tip %: ");
+        double tipPercent = input.nextInt();
+        tipPercent/=100; //Turn whole number into decimal
 
-        System.out.
+        //Getting tip
+        double tip = bill * tipPercent;
+
+        //Getting total
+        double total = bill + tip;
+
+        //Tip per person
+        double tipPerPerson = tip / people;
+
+        //Total per person
+        double totalPerPerson = total / people;
+
+        //Displaying all numbers, rounded to two decimal places
+        System.out.println("Tip: $" + String.format("%.2f", tip));
+        System.out.println("Total: $" + String.format("%.2f", total));
+        System.out.println("Tip per person: $" + String.format("%.2f", tipPerPerson));
+        System.out.println("Total per person: $" + String.format("%.2f", totalPerPerson));
     }
 }
