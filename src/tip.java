bee52e494065e4429021eff216a2834f76fc0b00
spedmond <114600476+spedmond@users.noreply.github.com>
2022-10-05 02:07:50 -0400

    Update tip.java
    comments

--- src/tip.java
+++ src/tip.java
@@ -10,37 +10,37 @@ public class tip {
         System.out.println("| TIP CALCULATOR |");
         System.out.println("------------------");
 
-        //User puts in cost of bill
+        //User puts in cost of bill, stores value in bill
         System.out.println("Please enter the total cost of your bill below.");
         System.out.print("Bill: $");
         double bill = input.nextDouble();
         System.out.println();
 
-        //User puts in amount of people
+        //User puts in amount of people, stores value in people
         System.out.println("Please enter the amount of people you dined with.");
         System.out.print("Amount of people: ");
         int people = input.nextInt();
         System.out.println();
 
-        //User puts in tip %
+        //User puts in tip %, stores value in tipPercent
         System.out.println("Please enter the percent you tipped your server AS A WHOLE NUMBER.");
         System.out.print("Tip %: ");
         double tipPercent = input.nextInt();
-        tipPercent/=100; //Turn whole number into decimal
+        tipPercent/=100; //Turn the whole number into a decimal in order to calculate the actual tip
         System.out.println();
 
 //-----------------------------------------------------------------------------------------------
 
-        //Calculating tip
+        //Calculating tip by multiplying the bill by the tip %
         double tip = bill * tipPercent;
 
-        //Calculating total
+        //Calculating total by adding the bill and tip together
         double total = bill + tip;
 
-        //Calculating tip per person
+        //Calculating tip per person by dividing the tip by the # of people
         double tipPerPerson = tip / people;
 
-        //Calculating total per person
+        //Calculating total per person by dividing the total by the # of people
         double totalPerPerson = total / people;
 
 //-----------------------------------------------------------------------------------------------
@@ -49,7 +49,7 @@ public class tip {
         System.out.println("---------");
         System.out.println("| COSTS |");
         System.out.println("---------");
-        
+
         System.out.println("Tip: $" + String.format("%.2f", tip));
         System.out.println("Total: $" + String.format("%.2f", total));
         System.out.println("Tip per person: $" + String.format("%.2f", tipPerPerson));
